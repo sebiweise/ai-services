90ab78e38e45268a323f4c94da6e888b846135fa
Sebastian Goscinski <sebiweise20@gmail.com>
2023-12-20 08:02:36 +0000

    Added shadcn/ui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Turborepo starter
+# AI-Services
 
 This is an official starter Turborepo.
 
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
-};
+const nextConfig = {
+    transpilePackages: ["@repo/ui"],
+}
+
+module.exports = nextConfig
