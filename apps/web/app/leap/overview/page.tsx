34dac04b758f3c744267081928566169b37402e8
Sebastian Goscinski <sebiweise20@gmail.com>
2024-01-05 13:24:30 +0000

    Leap overview update

--- apps/web/app/leap/overview/page.tsx
+++ apps/web/app/leap/overview/page.tsx
@@ -1,25 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Workflow } from "@prisma/client";
 import { columns } from "./columns"
 import { DataTable } from "./data-table"
-import { currentUser } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 
 const prisma = new PrismaClient()
 
-export default async function ModelPage() {
-    const user = await currentUser();
-
-    if (!user) {
-        return <div>User not found</div>;
-    }
-
-    const data = await prisma.workflow.findMany({
+async function getData(userId: string): Promise<Workflow[]> {
+    const workflows = await prisma.workflow.findMany({
         where: {
-            userId: user.id
+            userId: userId
         },
         include: {
             samples: true,
         },
     });
+
+    return workflows;
+}
+
+export default async function ModelPage() {
+    const { userId } = auth();
+
+    if (!userId) {
+        return <div>User not found</div>;
+    }
+
+    const data = await getData(userId)
 
     return (
         <div className="container mx-auto py-10">
