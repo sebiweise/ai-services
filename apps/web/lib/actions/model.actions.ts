a2bac60bd787449e5a1d9ddedc9a5af952264b67
Sebastian Goscinski <sebiweise20@gmail.com>
2024-01-09 08:03:56 +0000

    Added form actions

--- apps/web/lib/actions/model.actions.ts
+++ apps/web/lib/actions/model.actions.ts
@@ -1,8 +1,36 @@
 "use server"
 
-import { Model } from '@prisma/client';
+import { Model, Prisma } from '@prisma/client';
 import prisma from '../prisma';
 import { handleError } from '../utils';
+
+export async function createModel(model: Prisma.ModelUncheckedCreateInput) {
+    try {
+        const newModel = await prisma.model.create({
+            data: model
+        })
+
+        return newModel;
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function getModelById(modelId: number) {
+    try {
+        const model = await prisma.model.findUnique({
+            where: {
+                id: modelId
+            }
+        })
+
+        if (!model) throw new Error('Model not found')
+
+        return model;
+    } catch (error) {
+        handleError(error)
+    }
+}
 
 export async function getAllModels(): Promise<Model[]> {
     try {
--- apps/web/lib/actions/vendor.actions.ts
+++ apps/web/lib/actions/vendor.actions.ts
@@ -1,8 +1,36 @@
 "use server"
 
-import { Vendor } from '@prisma/client';
+import { Prisma, Vendor } from '@prisma/client';
 import prisma from '../prisma';
 import { handleError } from '../utils';
+
+export async function createVendor(vendor: Prisma.VendorUncheckedCreateInput) {
+    try {
+        const newVendor = await prisma.vendor.create({
+            data: vendor
+        })
+
+        return newVendor;
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function getVendorById(vendorId: number) {
+    try {
+        const vendor = await prisma.vendor.findUnique({
+            where: {
+                id: vendorId
+            }
+        })
+
+        if (!vendor) throw new Error('Vendor not found')
+
+        return vendor;
+    } catch (error) {
+        handleError(error)
+    }
+}
 
 export async function getAllVendors(): Promise<Vendor[]> {
     try {
